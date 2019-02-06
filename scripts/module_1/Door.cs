be7190e7000fc4b7460c23a28c4d7b7a8d6f9da0
David Jonas <davidjonasdesign@gmail.com>
2019-02-06 15:58:44 +0100

    Fixed the door rotation and removed unnecessary namespaces

--- scripts/module_1/Door.cs
+++ scripts/module_1/Door.cs
@@ -6,22 +6,23 @@ public class Door : MonoBehaviour, IInteractable
 {
     public int id;
     public bool open = false;
+    private float initialRotation;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialRotation = transform.rotation.eulerAngles.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (open && transform.rotation.eulerAngles.y < 80)
+        if (open && transform.rotation.eulerAngles.y < initialRotation + 80)
         {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 80, 0), 5);
-        } else if (!open && transform.rotation.eulerAngles.y > 0)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, initialRotation + 80, 0), 5);
+        } else if (!open && transform.rotation.eulerAngles.y > initialRotation)
         {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 0), 5);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, initialRotation, 0), 5);
         }
         
     }
--- scripts/module_1/InventoryUI.cs
+++ scripts/module_1/InventoryUI.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
