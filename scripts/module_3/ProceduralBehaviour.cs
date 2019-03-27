cf745918957aea75311346f4fa6a282715f78a50
David Jonas <davidjonasdesign@gmail.com>
2019-03-27 09:34:20 +0100

    Adding the changes on Procedural behaviour for the fix of the jagged terrain.

--- scripts/module_3/ProceduralBehaviour.cs
+++ scripts/module_3/ProceduralBehaviour.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class ProceduralBehaviour : MonoBehaviour
 {
     public static ProceduralBehaviour instace;
-    public float perlinSeed;
+    public float perlinSeedX, perlinSeedY;
     public int seed = 0;
     public GameObject module;
     public Terrain t;
@@ -35,7 +35,7 @@ public class ProceduralBehaviour : MonoBehaviour
     private void Start()
     {
         pt = new ProceduralTerrain(worldSize, worldSize, passes);
-        t.terrainData.size = new Vector3(worldSize*2, maxHeight, worldSize*2);
+        //t.terrainData.size = new Vector3(1000, maxHeight, 1000);
         t.terrainData.heightmapResolution = worldSize;
         Generate();
     }
@@ -53,12 +53,7 @@ public class ProceduralBehaviour : MonoBehaviour
         setSeed(seed);
         Debug.Log("We can generate " + int.MaxValue + " versions of this procedure");
         Debug.Log("Generating version: " + seed);
-        
-        foreach (var obj in GameObject.FindGameObjectsWithTag("Procedural"))
-        {
-            Destroy(obj);
-        }
-        
+
         pt.Generate();
 
         float[,] norm = pt.GetHeightsNormalized();
@@ -78,7 +73,7 @@ public class ProceduralBehaviour : MonoBehaviour
         mask.SetPixels(colors);
         mask.Apply();
         
-        t.terrainData.terrainLayers[0].diffuseTexture = mask;
+        //t.terrainData.terrainLayers[0].diffuseTexture = mask;
     }
 
     public void setSeed(int s)
@@ -88,6 +83,8 @@ public class ProceduralBehaviour : MonoBehaviour
         {
             seed = s;
         }
-        perlinSeed = Random.Range(0.0f, 1000000f);
+        perlinSeedX = Random.Range(0.0f, 1000f);
+        perlinSeedY = Random.Range(0.0f, 1000f);
+
     }
 }
